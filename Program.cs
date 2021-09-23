0613623d1c418122abd0deed7ff024edd9d0612b
aniketbhuse <90091773+aniketbhuse@users.noreply.github.com>
2021-09-23 11:17:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+using ConectionDl;
+namespace EmployeeAppraisal
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+           SqlConnect.Conection();
+        }
+    }
+}
